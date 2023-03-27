ed3a2d5e4d4a3ecf5e1f13d63d200316446fd23a
Ganesh Negi <ganesh.negi@finbii.com>
2023-03-27 20:17:31 +0530

    new changes

--- backend/Controller/writerContent.js
+++ backend/Controller/writerContent.js
@@ -1,7 +1,7 @@
+const { Op } = require("sequelize");
 const db = require("../Database")
 const WriterContent = db.writerContent;
 const catchAsyncError = require("../Middleware/catchAsyncError");
-const { Cloudinary } = require("../Utils/Cloudinary");
 const ErrorHandling = require("../Utils/ErrorHandling");
 
 exports.getAllWriterContent = catchAsyncError(async (req, res) => {
@@ -14,7 +14,7 @@ exports.getAllWriterContent = catchAsyncError(async (req, res) => {
 exports.deleteWriterContent = catchAsyncError(async (req, res) => {
     const { id } = req.params;
     const { writer_id } = req.params;
-    const data = await WriterContent.findOne({ where: { id,writer_id } });
+    const data = await WriterContent.findOne({ where: { id, writer_id } });
     if (!data) {
         return new ErrorHandling("Data Not Found", 404);
     } else {
@@ -26,26 +26,33 @@ exports.deleteWriterContent = catchAsyncError(async (req, res) => {
 exports.updateWriterContent = catchAsyncError(async (req, res) => {
     const { id } = req.params;
     const { writer_id } = req.params;
-    const { content,type } = req.body;
-    const data = await WriterContent.findOne({ where: { id ,writer_id} });
+    const { content, type } = req.body;
+    const data = await WriterContent.findOne({ where: { id, writer_id } });
     if (!data) {
         return new ErrorHandling("Data Not Found", 404);
     } else {
-        const data1 = await WriterContent.update({ content,type  }, { where: { id } })
+        const data1 = await WriterContent.update({ content, type }, { where: { id } })
         return res.status(200).json({ message: "Succesfully Updated" });
     }
 })
 
 exports.cerateWriterContent = catchAsyncError(async (req, res) => {
     const { writer_id } = req.params;
-    const { type,content,title } = req.body;
-    const data = await WriterContent.create({ writer_id,type,content,title });
+    const { type, content, title } = req.body;
+    const data = await WriterContent.create({ writer_id, type, content, title });
     return res.status(200).json({ message: "Succesfully Created" });
 })
 
 
-exports.getWriterType = catchAsyncError(async(req,res)=>{
-    const {writer_id,type} = req.params;
-    const data = await WriterContent.findAll({where:{writer_id,type}});
+exports.getWriterType = catchAsyncError(async (req, res) => {
+    const { writer_id, type } = req.params;
+    const data = await WriterContent.findAll({
+        where: {
+            [Op.and]: [
+                { writer_id },
+                { type }
+            ]
+        }
+    });
     return res.status(200).json({ message: data });
 })
