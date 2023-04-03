7644d1366e577dcb924c217010b7230c4f5af00a
Ganesh Negi <ganesh.negi@finbii.com>
2023-04-04 01:49:55 +0530

    console.log(response)

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const app = express();
 const dotenv = require('dotenv')
 dotenv.config({ path: "Config/config.env" })
 require("./Database/index")
-app.use(express.json({ limit: "50mb", extended: 'true' }))
+const cors = require('cors');
+app.use(cors());
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const userRouter = require("./Routes/userRoute");
 const teamRouter = require("./Routes/teamRoute");
 const competionRouter = require("./Routes/competionRoutes");
@@ -14,11 +18,7 @@ const writerRouter = require("./Routes/writerRoutes");
 const writerContent = require("./Routes/writerContent");
 const resultRouter = require("./Routes/resultRoutes");
 
-const cors = require('cors');
-app.use(cors());
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use("/", userRouter)
 app.use("/", teamRouter)
 app.use("/", competionRouter)
